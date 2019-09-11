e358fb30cf9c935bf68df8300f9cd9582c761dd9
Rei M <r.clarkemitchell@gmail.com>
2019-09-11 04:08:12 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,26 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+new_hash = {}
+data.each do |col_gend_loc, innerAttrValues|
+# p col_gend_loc
+# p innerAttrValues
+innerAttrValues.each do |actual_col_gend_loc, names|
+p "#{actual_col_gend_loc}: #{names}"
+ names.each do |indiv_names|
+#  p indiv_names
+ if !new_hash[indiv_names]
+ new_hash[indiv_names] = {}
+ end
+ if !new_hash[indiv_names][col_gend_loc]
+ new_hash[indiv_names][col_gend_loc] = []
+ end
+#  p   "#{actual_col_gend_loc}: #{indiv_names}"
+  #=> still within the names loop, so iteration[1] is still on color '**purple**'
+ # {"Theo" => {color => ['purple']},}
+ #still within the names loop, so iteration[2] is still on color '**purple**'
+  # {"Theo" => {color => ['purple']}, "Peter Jr." => {color => [purple]}....
+	new_hash[indiv_names][col_gend_loc] << actual_col_gend_loc
+ end
+end
+end
+p new_hash
 end
